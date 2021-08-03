bdebd16a84e9badf2db8e131a4ed9c5f762a711a
betacom <betacom@Betacom-PC250C.lan>
2021-08-03 16:32:44 +0200

    primo inserimento dati nel db

---
+++ gestionecorsi/SQL/dropddl.sql
@@ -0,0 +1,5 @@
+drop table corsi_corsisti cascade constraints;
+drop table amministratori cascade constraints;
+drop table corsi cascade constraints;
+drop table docenti cascade constraints;
+drop table corsisti cascade costraints;
---
+++ gestionecorsi/SQL/insertdml.sql
@@ -0,0 +1,5 @@
+insert into amministratori values(1, 'Admin', 'Admin', 'Pass');
+insert into docenti values(1, 'Anna', 'Verdi', '/cv/anna.jpeg');
+insert into docenti values(2, 'Massimo', 'Rossi', '/cv/massimo.jpeg');
+insert into docenti values(3, 'Paolo', 'Bianchi', '/cv/paolo.jpeg');
+insert into docenti values(4, 'Laura', 'Gialli', '/cv/laura.jpeg');
