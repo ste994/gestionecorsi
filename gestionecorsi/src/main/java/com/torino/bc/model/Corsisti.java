dcd040bd8ae67b3b9da009ed09b9ced7947a060d
betacom <betacom@Betacom-PC250C.fritz.box>
2021-08-04 10:24:12 +0200

    Corsisti commit

--- gestionecorsi/src/main/java/com/torino/bc/model/Corsisti.java
+++ gestionecorsi/src/main/java/com/torino/bc/model/Corsisti.java
@@ -1,5 +1,34 @@
 package com.torino.bc.model;
 
 public class Corsisti {
-
+	private long codCorsista;
+	private String nomeCorsista;
+	private String cognomeCorsista;
+	private String precedentiFormativi;
+	public long getCodCorsista() {
+		return codCorsista;
+	}
+	public void setCodCorsista(long codCorsista) {
+		this.codCorsista = codCorsista;
+	}
+	public String getNomeCorsista() {
+		return nomeCorsista;
+	}
+	public void setNomeCorsista(String nomeCorsista) {
+		this.nomeCorsista = nomeCorsista;
+	}
+	public String getCognomeCorsista() {
+		return cognomeCorsista;
+	}
+	public void setCognomeCorsista(String cognomeCorsista) {
+		this.cognomeCorsista = cognomeCorsista;
+	}
+	public String getPrecedentiFormativi() {
+		return precedentiFormativi;
+	}
+	public void setPrecedentiFormativi(String precedentiFormativi) {
+		this.precedentiFormativi = precedentiFormativi;
+	}
+	
+	
 }
