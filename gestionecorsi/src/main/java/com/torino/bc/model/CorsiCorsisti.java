074994428e5e0d498508340452a126ed136f347b
betacom <betacom@Betacom-PC801B.homenet.telecomitalia.it>
2021-08-04 11:59:08 +0200

    Corsi-Corsisti e Docenti Model aggiunti

---
+++ gestionecorsi/src/main/java/com/torino/bc/model/CorsiCorsisti.java
@@ -0,0 +1,26 @@
+package com.torino.bc.model;
+
+public class CorsiCorsisti {
+    private long codcc;
+    private long codcorsista;
+    private long codcorso;
+
+    public long getCodcc() {
+        return codcc;
+    }
+    public void setCodcc(long codcc) {
+        this.codcc = codcc;
+    }
+    public long getCodcorsista() {
+        return codcorsista;
+    }
+    public void setCodcorsista(long codcorsista) {
+        this.codcorsista = codcorsista;
+    }
+    public long getCodcorso() {
+        return codcorso;
+    }
+    public void setCodcorso(long codcorso) {
+        this.codcorso = codcorso;
+    }
+}
--- gestionecorsi/src/main/java/com/torino/bc/model/Docenti.java
+++ gestionecorsi/src/main/java/com/torino/bc/model/Docenti.java
@@ -2,4 +2,35 @@ package com.torino.bc.model;
 
 public class Docenti {
 
+    private long coddocente;
+    private String nomeDocente ;
+    private String cognomeDocente;
+    private String cvdocente;
+
+    public long getCoddocente() {
+        return coddocente;
+    }
+    public void setCoddocente(long coddocente) {
+        this.coddocente = coddocente;
+    }
+    public String getNomeDocente() {
+        return nomeDocente;
+    }
+    public void setNomeDocente(String nomeDocente) {
+        this.nomeDocente = nomeDocente;
+    }
+    public String getCognomeDocente() {
+        return cognomeDocente;
+    }
+    public void setCognomeDocente(String cognomeDocente) {
+        this.cognomeDocente = cognomeDocente;
+    }
+    public String getCvdocente() {
+        return cvdocente;
+    }
+    public void setCvdocente(String cvdocente) {
+        this.cvdocente = cvdocente;
+    }
 }
+
+
