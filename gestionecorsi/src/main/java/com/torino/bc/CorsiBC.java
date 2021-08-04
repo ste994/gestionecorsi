0a6799f54647c92d5491a6db4413b67b0a8de747
Betacom <Betacom@Betacom-PC828F>
2021-08-04 12:04:01 +0200

    Aggiunto CorsiBC

---
+++ gestionecorsi/src/main/java/com/torino/bc/CorsiBC.java
@@ -0,0 +1,54 @@
+package com.torino.bc;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.torino.architecture.dao.CorsiDAO;
+import com.torino.architecture.dao.DAOException;
+import com.torino.architecture.dbaccess.DBAccess;
+import com.torino.bc.model.Corsi;
+
+public class CorsiBC {
+	private Connection conn;
+
+	public CorsiBC() throws DAOException, ClassNotFoundException, IOException{
+			conn = DBAccess.getConnection();
+}
+
+	public void createOrUpdate(Corsi c) throws DAOException, ClassNotFoundException, IOException {
+		try {
+			if (c.getCodCorso() > 0) { 
+				CorsiDAO.getFactory().update(conn, c);
+			} else {
+			 CorsiDAO.getFactory().create(conn, c);
+			}
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+
+	public void delete(Corsi c) throws DAOException {
+		try {
+			CorsiDAO.getFactory().delete(conn, c);
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+
+	public Corsi[] getCorsi() throws DAOException {
+		try {
+			return CorsiDAO.getFactory().getAll(conn);
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+
+	public Corsi getById(long id) throws DAOException {
+		try {
+			return CorsiDAO.getFactory().getId(conn, id);
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+}
