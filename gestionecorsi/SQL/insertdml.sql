2fc3e538aa31123b79b564815f1ca78ca6e4a6a1
betacom <betacom@Betacom-PC801B.homenet.telecomitalia.it>
2021-08-05 09:42:54 +0200

    Elimina istruzione truncate

--- gestionecorsi/SQL/insertdml.sql
+++ gestionecorsi/SQL/insertdml.sql
@@ -3,5 +3,3 @@ insert into docenti values(1, 'Anna', 'Verdi', '/cv/anna.jpeg');
 insert into docenti values(2, 'Massimo', 'Rossi', '/cv/massimo.jpeg');
 insert into docenti values(3, 'Paolo', 'Bianchi', '/cv/paolo.jpeg');
 insert into docenti values(4, 'Laura', 'Gialli', '/cv/laura.jpeg');
-
-truncate table amministratori;
